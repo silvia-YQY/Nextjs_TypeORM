277c0085f3f53ef73edd4caf2d45468fd8bfdfba
silvia-YQY <545067444@qq.com>
2020-07-04 21:43:44 +0800

    add posts table

---
+++ dist/migration/1593847669511-CreatePost.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CreatePost1593847669511 = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var _typeorm = require("typeorm");
+
+var CreatePost1593847669511 = /*#__PURE__*/function () {
+  function CreatePost1593847669511() {
+    (0, _classCallCheck2["default"])(this, CreatePost1593847669511);
+  }
+
+  (0, _createClass2["default"])(CreatePost1593847669511, [{
+    key: "up",
+    value: function () {
+      var _up = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(queryRunner) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                // 升级数据库
+                queryRunner.createTable(new _typeorm.Table({
+                  name: 'posts',
+                  columns: [{
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment"
+                  }, {
+                    name: 'title',
+                    type: 'varchar'
+                  }, {
+                    name: 'content',
+                    type: 'text'
+                  }]
+                }));
+
+              case 1:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function up(_x) {
+        return _up.apply(this, arguments);
+      }
+
+      return up;
+    }()
+  }, {
+    key: "down",
+    value: function () {
+      var _down = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(queryRunner) {
+        return _regenerator["default"].wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                // 降级数据库
+                queryRunner.dropTable('posts');
+
+              case 1:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2);
+      }));
+
+      function down(_x2) {
+        return _down.apply(this, arguments);
+      }
+
+      return down;
+    }()
+  }]);
+  return CreatePost1593847669511;
+}();
+
+exports.CreatePost1593847669511 = CreatePost1593847669511;
---
+++ src/migration/1593847669511-CreatePost.ts
@@ -0,0 +1,30 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreatePost1593847669511 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // 升级数据库
+        return await queryRunner.createTable(new Table({
+            name: 'posts',
+            columns: [{
+                name: 'id',
+                type: 'int',
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: "increment"
+            },{
+                name: 'title',
+                type: 'test'
+            },{
+                name: 'content',
+                type: 'text'
+            }]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // 降级数据库
+        return await queryRunner.dropTable('posts')
+    }
+
+}
