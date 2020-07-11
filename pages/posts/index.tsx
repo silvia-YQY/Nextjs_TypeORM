bd93acc1e12d77b8ed369934300cd928cdacb504
silvia-YQY <545067444@qq.com>
2020-07-11 11:29:54 +0800

    创建三个实体及其关联

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,25 +1,26 @@
-import {GetStaticProps, NextPage} from 'next';
-import {usePosts} from '../../hooks/usePosts';
-import {useCallback} from 'react';
-import {getPosts} from '../../lib/posts';
+import { GetStaticProps, NextPage } from 'next';
+import { usePosts } from '../../hooks/usePosts';
+import { useCallback } from 'react';
+import { getPosts } from '../../lib/posts';
 import Link from 'next/link';
 
 type Props = {
   posts: Post[];
-}
+};
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts} = props;
+  const { isLoading, isEmpty } = usePosts();
+  const { posts } = props;
   console.log(posts);
   return (
     <div>
       <h1>文章列表</h1>
-      {posts.map(p => <div key={p.id}>
-        <Link href={`/posts/${p.id}`}>
-          <a>
-            {p.id}
-          </a>
-        </Link>
-      </div>)}
+      {posts.map((p) => (
+        <div key={p.id}>
+          <Link href={`/posts/${p.id}`}>
+            <a>{p.id}</a>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
@@ -30,7 +31,7 @@ export const getStaticProps = async () => {
   const posts = await getPosts();
   return {
     props: {
-      posts: JSON.parse(JSON.stringify(posts))
-    }
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
   };
 };
