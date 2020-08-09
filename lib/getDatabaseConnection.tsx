8e2f6feb9974b70453722111dd10bf8be7068ea6
silvia-YQY <545067444@qq.com>
2020-08-09 17:17:17 +0800

    fix: metadata for post

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,17 +1,25 @@
 import {createConnection, getConnectionManager} from "typeorm";
+import 'reflect-metadata'
+import config from 'ormconfig.json'
+import {Post} from 'src/entity/Post'
+import {User} from 'src/entity/User'
+import {Comment} from 'src/entity/Comment'
+
+const create = async () =>{
+    // @ts-ignore
+    return createConnection({
+        ...config,
+        entities: [Post, User, Comment]
+    });
+}
 
 const promise = (async function(){
     const manager = getConnectionManager()
-    if(!manager.has('default')){
-        return createConnection()
-    }else{
-        const current = manager.get('default')
-        if(current.isConnected){
-            return current;
-        }else{
-            return createConnection()
-        }
+    const current = manager.has('default') && manager.get('default')
+    if(current && current.isConnected){
+        await current.close()
     }
+    return create();
 })()
 
 export const getDatabaseConnection = async () =>{
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,34 @@ import {UAParser} from 'ua-parser-js';
 import {useEffect, useState} from 'react';
 import {createConnection, getConnection} from "typeorm";
 import { getDatabaseConnection } from 'lib/getDatabaseConnection';
+import {Post} from "../src/entity/Post";
 
 type Props = {
-  browser: {
-    name: string;
-    version: string;
-    major: string;
-  }
+  posts:Post[]
 }
 const index: NextPage<Props> = (props) => {
-  const {browser} = props;
-  const [width, setWidth] = useState(0);
-  useEffect(() => {
-    const w = document.documentElement.clientWidth;
-    setWidth(w);
-  }, []);
+  const {posts} = props;
+
+  console.log('post',posts)
   return (
     <div>
-      <h1>你的浏览器是 {browser.name}</h1>
-      <h2>你的浏览器窗口大小是:{width} 像素</h2>
+      {posts.map(post =>{
+        <div key={post.id}>{post.title}</div>
+      })}
     </div>
   );
 };
-export default index;
 
+export default index;
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connect = await getDatabaseConnection();
+  const connection = await getDatabaseConnection();
+  const posts = await connection.manager.find(Post)
   const ua = context.req.headers['user-agent'];
   const result = new UAParser(ua).getResult();
   return {
     props: {
-      browser: result.browser
+      browser: result.browser,
+      posts: JSON.parse(JSON.stringify(posts))
     }
   };
 };
