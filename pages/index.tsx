60bd58b69dc29fe3673872e7672853ea794fe395
silvia-YQY <545067444@qq.com>
2020-08-09 18:01:37 +0800

    成功展示post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
 import {GetServerSideProps, NextPage} from 'next';
 import {UAParser} from 'ua-parser-js';
-import {useEffect, useState} from 'react';
-import {createConnection, getConnection} from "typeorm";
+
 import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 import {Post} from "../src/entity/Post";
+import Link from "next/link";
 
 type Props = {
   posts:Post[]
 }
 const index: NextPage<Props> = (props) => {
   const {posts} = props;
-
-  console.log('post',posts)
+  console.log('post33',posts)
   return (
     <div>
-      {posts.map(post =>{
-        <div key={post.id}>{post.title}</div>
-      })}
+      <h1>文章列表</h1>
+      {posts.map(post =>
+        <Link key={post.id} href={`/posts/${post.id}`}>
+          <a>{post.title}</a>
+        </Link>
+      )}
     </div>
   );
 };
@@ -25,11 +27,8 @@ export default index;
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const connection = await getDatabaseConnection();
   const posts = await connection.manager.find(Post)
-  const ua = context.req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
   return {
     props: {
-      browser: result.browser,
       posts: JSON.parse(JSON.stringify(posts))
     }
   };
