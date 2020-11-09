700e62bf2686541409d8320282d577d874d596ac
silvia-YQY <545067444@qq.com>
2020-11-09 22:17:27 +0800

    add：migration/1604930066240-AddUniqueUsernameToUsers

---
+++ dist/migration/1604930066240-AddUniqueUsernameToUsers.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.AddUniqueUsernameToUsers1604930066240 = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var _typeorm = require("typeorm");
+
+var AddUniqueUsernameToUsers1604930066240 = /*#__PURE__*/function () {
+  function AddUniqueUsernameToUsers1604930066240() {
+    (0, _classCallCheck2["default"])(this, AddUniqueUsernameToUsers1604930066240);
+  }
+
+  (0, _createClass2["default"])(AddUniqueUsernameToUsers1604930066240, [{
+    key: "up",
+    value: function () {
+      var _up = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(queryRunner) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return queryRunner.createIndex('users', new _typeorm.TableIndex({
+                  name: 'users_username',
+                  columnNames: ['username'],
+                  isUnique: true
+                }));
+
+              case 2:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function up(_x) {
+        return _up.apply(this, arguments);
+      }
+
+      return up;
+    }()
+  }, {
+    key: "down",
+    value: function () {
+      var _down = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(queryRunner) {
+        return _regenerator["default"].wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return queryRunner.dropIndex('users', 'users_username');
+
+              case 2:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2);
+      }));
+
+      function down(_x2) {
+        return _down.apply(this, arguments);
+      }
+
+      return down;
+    }()
+  }]);
+  return AddUniqueUsernameToUsers1604930066240;
+}();
+
+exports.AddUniqueUsernameToUsers1604930066240 = AddUniqueUsernameToUsers1604930066240;
