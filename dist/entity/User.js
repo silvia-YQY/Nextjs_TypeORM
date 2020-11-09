ced6e9ff95a4ab858de5804286d3a7c24269d85f
silvia-YQY <545067444@qq.com>
2020-11-09 23:37:57 +0800

    feat: 添加 toJSON 方法，过滤user序列化时隐藏敏感和不必要的信息

--- dist/entity/User.js
+++ dist/entity/User.js
@@ -33,6 +33,8 @@ var _getDatabaseConnection = require("../../lib/getDatabaseConnection");
 
 var _md = _interopRequireDefault(require("md5"));
 
+var _lodash = _interopRequireDefault(require("lodash"));
+
 var _dec, _dec2, _dec3, _dec4, _dec5, _dec6, _dec7, _dec8, _dec9, _class, _class2, _descriptor, _descriptor2, _descriptor3, _descriptor4, _descriptor5, _descriptor6, _descriptor7, _temp;
 
 var User = (_dec = (0, _typeorm.Entity)('users'), _dec2 = (0, _typeorm.PrimaryGeneratedColumn)('increment'), _dec3 = (0, _typeorm.Column)('varchar'), _dec4 = (0, _typeorm.Column)('varchar'), _dec5 = (0, _typeorm.CreateDateColumn)(), _dec6 = (0, _typeorm.UpdateDateColumn)(), _dec7 = (0, _typeorm.OneToMany)(function (type) {
@@ -137,6 +139,11 @@ var User = (_dec = (0, _typeorm.Entity)('users'), _dec2 = (0, _typeorm.PrimaryGe
     key: "generatePasswordDigest",
     value: function generatePasswordDigest() {
       this.passwordDigest = (0, _md["default"])(this.password);
+    }
+  }, {
+    key: "toJSON",
+    value: function toJSON() {
+      return _lodash["default"].omit(this, ['password', 'passwordConfirmation', 'passwordDigest']);
     }
   }]);
   return User;
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -11,6 +11,7 @@ import {Post} from "./Post";
 import {Comment} from "./Comment";
 import {getDatabaseConnection} from "../../lib/getDatabaseConnection";
 import md5 from "md5";
+import _ from 'lodash';
 
 @Entity('users')
 export class User {
@@ -69,5 +70,8 @@ export class User {
     @BeforeInsert()
     generatePasswordDigest() {
         this.passwordDigest = md5(this.password);
+    }
+    toJSON() {
+        return _.omit(this, ['password', 'passwordConfirmation', 'passwordDigest']);
     }
 }
